47897012a0c89d3efa946dcb8a32b0e3859673d8
ZakharBlinov <blinov.0121z@gmail.com>
2024-02-07 12:01:28 +0300

    Установлен пакет с помощью npm link

---
+++ bin/brain-even.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}! Answer "yes" if the number is even, otherwise answer "no".`);
+
+let correctAnswersCount = 0;
+
+while (correctAnswersCount < 3) {
+  const randomNumber = Math.floor(Math.random() * 100);
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  const isEven = randomNumber % 2 === 0;
+  const correctAnswer = isEven ? 'yes' : 'no';
+
+  if (userAnswer.toLowerCase() === correctAnswer) {
+    console.log('Correct!');
+    correctAnswersCount += 1;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`);
+    break;
+  }
+}
+
+if (correctAnswersCount === 3) {
+  console.log(`Congratulations, ${name}!`);
+}
+

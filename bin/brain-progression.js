163bdb83369bac4d2177f006f9ed2dc8b87147ed
ZakharBlinov <blinov.0121z@gmail.com>
2024-02-08 13:25:10 +0300

    project

---
+++ bin/brain-progression.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const generateProgression = (length, start, difference) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + difference * i);
+  }
+  return progression;
+};
+
+const playProgressionGame = (name) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${name}! What number is missing in the progression?`);
+
+  let correctAnswersCount = 0;
+  const roundsToWin = 3;
+
+  while (correctAnswersCount < roundsToWin) {
+    const length = Math.floor(Math.random() * 6) + 5;
+    const start = Math.floor(Math.random() * 20);
+    const difference = Math.floor(Math.random() * 10) + 1;
+    const hiddenIndex = Math.floor(Math.random() * length);
+
+    const progression = generateProgression(length, start, difference);
+    const correctAnswer = progression[hiddenIndex];
+    progression[hiddenIndex] = '..';
+    
+    console.log(`Question: ${progression.join(' ')}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    
+    if (Number(userAnswer) === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      break;
+    }
+  }
+
+  if (correctAnswersCount === roundsToWin) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+const playerName = readlineSync.question('May I have your name? ');
+playProgressionGame(playerName);
+

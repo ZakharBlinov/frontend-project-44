bd17194e7b022366909cb5f253bc75aa61d9f78a
ZakharBlinov <blinov.0121z@gmail.com>
2024-02-14 14:52:24 +0300

    final

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,24 +3,4 @@
 
 import greetUser from '../src/cli.js';
 
-import playCalculatorGame from '../src/games/brain-calc.js';
-
-import playGCDGame from '../src/games/brain-gcd.js';
-
-import PlayPrimeGame from '../src/games/brain-prime.js';
-
-import playProgressionGame from '../src/games/brain-progression.js';
-
-import playEvenGame from '../src/games/brain-even.js';
-
 greetUser();
-
-playCalculatorGame();
-
-playGCDGame();
-
-PlayPrimeGame();
-
-playProgressionGame();
-
-playEvenGame();

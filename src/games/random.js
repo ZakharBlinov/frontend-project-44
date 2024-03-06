a9d807dae6c1305eb7706e574ead2e8efa611092
ZakharBlinov <blinov.0121z@gmail.com>
2024-03-06 15:57:15 +0300

    project

--- src/games/random.js
+++ src/games/random.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-shadow */
 const getRandomNumber = (min, max) => {
-    const getRandomNumber = Math.floor(Math.random() * (max - min) + min);
-    return getRandomNumber;
-  };
-  
-  export default getRandomNumber;
+  const getRandomNumber = Math.floor(Math.random() * (max - min) + min);
+  return getRandomNumber;
+};
+
+export default getRandomNumber;
